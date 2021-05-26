aaadbf7aa61c6b253cd1c170de155a81d2c14d32
Natasha Gurova <78147880+Tulenenok@users.noreply.github.com>
2021-05-26 20:55:09 +0300

    Update mct_pattern.py

--- p2/animation/mct_pattern.py
+++ p2/animation/mct_pattern.py
@@ -1,13 +1,20 @@
+WINDOW_COLOR = (255, 255, 255)
+
 import pygame
 
 pygame.init()
-window = pygame.display.set_mode((300, 200))
+window = pygame.display.set_mode((600, 600))
 pygame.display.set_caption('АААААААААААААААААА')
 
 run = True
 while run:
     pygame.time.delay(50)
-    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    window.fill(WINDOW_COLOR)
+    pygame.display.update()
+
+pygame.quit()
